3c6278b20bdd22542b7448bc4a58a442654cba11
TaekMin LEE <uh06056@gmail.com>
2025-09-23 23:05:00 +0900

    feat: Report, ReportsPost 도메인,DTO,Repository 추가

---
+++ src/main/java/com/example/swproject/dto/ReportsPostDto.java
@@ -0,0 +1,7 @@
+package com.example.swproject.dto;
+
+/**
+ * DTO for {@link com.example.swproject.domain.ReportsPost}
+ */
+public class ReportsPostDto {
+  }
---
+++ src/main/java/com/example/swproject/repository/SDJpaReportsPostRepository.java
@@ -0,0 +1,4 @@
+package com.example.swproject.repository;
+
+public interface SDJpaReportsPostRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.swproject.domain.ReportsPost, java.lang.Long> {
+  }
