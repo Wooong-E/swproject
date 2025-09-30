35fb683deab9aac8703fc08da0fe45e9961a3cdd
Seosangbeom <tjtkdqja1215@naver.com>
2025-09-30 21:26:31 +0900

    feat:프론트수정

--- src/main/java/com/example/swproject/security/SecurityConfig.java
+++ src/main/java/com/example/swproject/security/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/users/login", "/users/signup", "/css/**", "/js/**", "/images/**", "/uploads/**", "/attractions", "/attractions/**", "/restaurants", "/restaurants/**", "/cafes", "/cafes/**","/reviews/**", "/my-place").permitAll()
+                        .requestMatchers("/", "/users/login", "/users/signup", "/css/**", "/js/**", "/images/**", "/uploads/**", "/attractions", "/attractions/**", "/restaurants", "/restaurants/**", "/cafes", "/cafes/**","/reviews/**", "/my-place", "/suggest", "/report").permitAll()
                         .requestMatchers("/courses/**").authenticated()
                         .anyRequest().authenticated()
                 )
