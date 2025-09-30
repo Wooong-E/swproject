c07134027202210b254f23c3e821258b5f26b1b1
JoWoong <whdnd5725@gmail.com>
2025-09-30 19:05:10 +0900

    fix: merge 중 conflict 해결 및 코드 최적화

--- src/main/java/com/example/swproject/domain/Review.java
+++ src/main/java/com/example/swproject/domain/Review.java
@@ -5,11 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
 import java.time.LocalDateTime;
 
 @Getter
